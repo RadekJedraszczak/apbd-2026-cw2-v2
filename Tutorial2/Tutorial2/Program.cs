5fd7bfc68ac0e6ac51a2c5f876f76c6ef32f25e3
RadekJedraszczak <s32847@pjwstk.edu.pl>
2026-03-12 09:54:34 +0100

    new method CalculateAverage

--- Tutorial2/Tutorial2/Program.cs
+++ Tutorial2/Tutorial2/Program.cs
@@ -16,10 +16,34 @@ try
 {
     int x = int.Parse(Console.In.ReadLine());
     Console.WriteLine(x*x);
+    double[] y = new[] { 1.0, 2.0, 3.0, 4.0};
+    double z = 0;
+    for (int i = 0; i < y.Length; i++)
+    {
+        z += y[i];
+    }
+    Console.WriteLine(z);
 }
 catch (Exception e)
 {
     Console.WriteLine(e);
     throw;
 }
+
+static int CalculateAverage(int[] values)
+{
+    int sum = 0;
+    for (int i = 0; i < values.Length; i++)
+    {
+        sum += values[i];
+    }
+    sum /= values.Length;
+    return sum;
+}
+
+int[] val = new[]{1,2,3,4,5,6};
+
+Console.WriteLine(CalculateAverage(val));
+
+
 
