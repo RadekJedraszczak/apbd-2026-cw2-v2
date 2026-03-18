17feb970e7b21167017f9d8eaa2d8a4d4ce2c01b
RadekJedraszczak <s32847@pjwstk.edu.pl>
2026-03-18 20:28:28 +0100

    bigValue change

--- Tutorial2/Tutorial2/Program.cs
+++ Tutorial2/Tutorial2/Program.cs
@@ -46,9 +46,9 @@ static int CalculateAverage(int[] values)
     return sum;
 }
 
-int[] val = new[]{1,2,3,4,5,6};
+int[] bigValues = new[]{1,2,3,4,5,6};
 
-Console.WriteLine(CalculateAverage(val));
+Console.WriteLine(CalculateAverage(bigValues));
 
 int CalculateMin(int[] values)
 {
